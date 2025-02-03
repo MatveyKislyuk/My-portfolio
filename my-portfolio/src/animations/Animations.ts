f198e9754112d93cc786e1ab20a74206681ddefd
Matvey Kislyuk <lologan228@yandex.ru>
2025-02-03 19:32:00 +1000

    Creating themes, improving appearance and adaptability, using animations

---
+++ my-portfolio/src/animations/Animations.ts
@@ -0,0 +1,19 @@
+export const pageTransition = {
+    initial: { opacity: 0 },
+    animate: { opacity: 1 },
+    exit: { opacity: 0 },
+    transition: { duration: 0.5 },
+};
+
+export const buttonHoverAnimation = {
+    whileHover: { scale: 1.3 },
+    transition: { type: 'spring', stiffness: 300 },
+};
+
+
+export const themeIconAnimation = {
+    initial: { opacity: 0 },
+    animate: { opacity: 1 },
+    exit: { opacity: 0 },
+    transition: { duration: 0.9 },
+};
